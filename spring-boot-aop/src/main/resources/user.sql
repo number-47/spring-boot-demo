da31c3a270f574c1f61e7801b209efc62a3534c3
number-47 <820368938@qq.com>
2019-07-17 18:48:02 +0800

    spring boot 与mybatis spring boot 发布webservice spring boot aop操作日志

---
+++ spring-boot-aop/src/main/resources/user.sql
@@ -0,0 +1,41 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : local
+ Source Server Type    : MySQL
+ Source Server Version : 50515
+ Source Host           : localhost:3306
+ Source Schema         : test
+
+ Target Server Type    : MySQL
+ Target Server Version : 50515
+ File Encoding         : 65001
+
+ Date: 24/06/2019 14:49:05
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+  `name` varchar(30) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL COMMENT '姓名',
+  `age` int(11) NULL DEFAULT NULL COMMENT '年龄',
+  `email` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL COMMENT '邮箱',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES (1, 'new', 14, '888888@qq.com');
+INSERT INTO `user` VALUES (2, 'Jack', 20, 'test2@baomidou.com');
+INSERT INTO `user` VALUES (3, 'Tom', 28, 'test3@baomidou.com');
+INSERT INTO `user` VALUES (4, 'Sandy', 21, 'test4@baomidou.com');
+INSERT INTO `user` VALUES (5, 'new2', 17, '88889988@qq.com');
+
+SET FOREIGN_KEY_CHECKS = 1;
---
+++ spring-boot-mybatis/ReadMe.md
@@ -0,0 +1,14 @@
+# spring boot 与 Mybatis的整合
+对User表的Crud
+ 
+Rest方式：
+
+| 请求方式 | url            | 功能说明   |
+| -------- | -------------- | -------------- |
+| get      | /getUser/id    | 根据id获取用户 |
+| post     | /addUser       | 增加用户   |
+| put      | /updateUser    | 根据id更新用户 |
+| delete   | /deleteUser/id | 根据id删除用户 |
+
+## druid的访问
+http://localhost:8111/druid/login.html
